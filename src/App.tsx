3326b7228bee0526050e3c4a2772d2e58fee1c46
Doguhan <doguhaneroljob@gmail.com>
2024-09-13 17:02:28 +0300

    Project started.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <button className="btn btn-primary">DaisyUI Button</button>
+    </div>
+  );
+}
+
+export default App;
